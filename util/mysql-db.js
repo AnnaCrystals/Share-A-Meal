7c6b4cf22a1a97161aed62ada7abe429743a7300
Daan de Vries <jumpsuitfrost@gmail.com>
2023-04-26 12:49:11 +0200

    Added .env

--- util/mysql-db.js
+++ util/mysql-db.js
@@ -1,16 +1,25 @@
 // get the client
 const mysql = require('mysql2');
+require('dotenv').config();
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_DATABASE:', process.env.DB_DATABASE);
+
 
 // create the connection to database
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'shareameal',
+    host: process.env.DB_HOST, //|| 'localhost',
+    user: process.env.DB_USER, //|| 'root',
+    database: process.env.DB_DATABASE, //|| 'shareameal',
     waitForConnections: true,
     connectionLimit: 10,
+    multipleStatements: true,
     maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
     idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-    queueLimit: 0
+    queueLimit: 0,
+    //port: process.env.DB_PORT, //|| 3306,
+    //password: process.env.DB_PASSWORD || '',
 });
 
 pool.getConnection(function (err, conn) {
@@ -29,8 +38,5 @@ pool.getConnection(function (err, conn) {
         pool.releaseConnection(conn);
     }
 });
-
-
-
 
 module.exports = pool;
