899785ae4a58ce4c5e8f89b8c9339329a019e8c7
Daan de Vries <jumpsuitfrost@gmail.com>
2023-05-11 19:04:14 +0200

    Changed database

--- util/mysql-db.js
+++ util/mysql-db.js
@@ -10,7 +10,7 @@ console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 
 // create the connection to database
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '127.0.0.1',
     user: process.env.DB_USER || 'root',
     database: process.env.DB_DATABASE || 'shareameal',
     waitForConnections: true,
