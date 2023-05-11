8105688634450b1d9ca136ff19fdf5bb4548d7a4
Daan de Vries <jumpsuitfrost@gmail.com>
2023-05-11 18:38:48 +0200

    Changed dependcy

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/info', infoRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
