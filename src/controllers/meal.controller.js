ccf67e49dad8852ec7997d1bfc2b6b611c49dd7d
Daan de Vries <jumpsuitfrost@gmail.com>
2023-05-21 08:11:19 +0200

    Railway testing

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -41,11 +41,11 @@ const mealController = {
         console.log('Creating a meal');
 
         //let dateTime = DATE_FORMATER(new Date(), "yyyy-mm-dd HH:MM:ss");
-        const date = new Date();
-        const dateTime = date.toISOString();
-        console.log(dateTime);
+        // const date = new Date();
+        // const dateTime = date.toISOString();
+        // console.log(dateTime);
 
-        const newMeal = { isActive, isVega, isVegan, isToTakeHome, dateTime: req.body.dateTime, maxAmountOfParticipants, price, imageUrl, cookId, name, description, allergenes } = { ...req.body, cookId: req.userId };
+        const newMeal = { isActive, isVega, isVegan, isToTakeHome, dateTime, maxAmountOfParticipants, price, imageUrl, cookId, name, description, allergenes } = { ...req.body, cookId: req.userId };
         try {
             console.log('Validating input data');
             assert(typeof isActive === 'number', 'isActive must be a number');
